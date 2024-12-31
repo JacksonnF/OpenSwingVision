b4fa638c5a7475ed29e4b7415aba2f10e0aa90ca
JacksonnF <jacksonfraser55@gmail.com>
2024-12-30 21:36:34 -0800

    initial video clipping

---
+++ utils.py
@@ -0,0 +1,15 @@
+import cv2
+
+
+def read_video(path_video):
+    cap = cv2.VideoCapture(path_video)
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    frames = []
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            frames.append(frame)
+        else:
+            break
+    cap.release()
+    return frames, fps
